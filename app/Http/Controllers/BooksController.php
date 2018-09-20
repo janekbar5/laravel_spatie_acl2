9c72b3a6080561c63b26f8aca58b1ad0024eb776
janekingeniousio <janek@ingeniousio.com>
2018-09-20 11:33:46 +0100

    ok

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,154 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class BooksController extends Controller
+
+{
+    protected $categoryObject;
+
+
+	public function __construct(CategoriesController $categoryObject)
+    {
+        $this->middleware('auth');
+	$this->categoryObject = $categoryObject;
+    }
+	
+	
+	
+	/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$books = Book::all();
+		//$books = Book::with('bookHasCategory')->get();
+		$books = Book::paginate(5);
+        return view('books.index',compact('books'));
+			//return 'rrrrrrrrrrrrrrrrr';
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        
+        //dd($request->all());
+        $book = new Book;        
+        $book->title = $request->input('title');
+        $book->category_id = $request->input('category_id');
+        $book->description = $request->input('description');
+
+        
+        //Book::create($book);
+        $book->save();
+
+
+        return redirect()->route('books.index')
+                        ->with('success','Book created successfully.');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::find($id);
+	return view('books.show',compact('book'));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)	
+    {
+        $book = Book::find($id);
+	return view('books.edit',compact('book'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $book = Book::find($id);
+        //dd($request->all());
+        $book->title = $request->input('title');
+        $book->category_id = $request->input('category_id');
+        $book->description = $request->input('description');
+        
+        $book->update();
+
+
+        return redirect()->route('books.index')
+                         ->with('success','Book updated successfully');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+		$book = Book::find($id);
+		$book->delete();
+        return redirect()->route('books.index')
+                         ->with('success','Book deleted successfully');
+		
+		//return 'eeeee';
+    }
+	
+	
+	
+	
+}
---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,124 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class CategoriesController extends Controller
+
+{
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
+	
+	
+	
+	
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index',compact('categories'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+			//return 'rrrrrrrrrrrrrrrrr';
+    }
+
+
+    
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+
+    
+	
+	
+    public function store(Request $request)
+    {
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+
+        //Category::create($request->all());
+        $category = new Category;        
+        $category->title = $request->input('title');
+        $category->description = $request->input('description');
+        //Book::create($book);
+        $category->save();
+
+
+        return redirect()->route('categories.index')
+                        ->with('success','Category created successfully.');
+    }
+
+
+    
+	
+    public function show($id)
+    {
+        
+		$category = Category::find($id);
+		return view('categories.show',compact('category'));
+    }
+	
+	
+
+
+    
+    public function edit($id)	
+    {
+        $category = Category::find($id);
+		return view('categories.edit',compact('category'));
+    }
+
+
+   
+    public function update(Request $request, $id)
+    {
+         request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $category = Category::find($id);
+        
+        $category->title = $request->input('title');
+        $category->description = $request->input('description');
+        //Book::create($book);
+        $category->save();
+        
+        //$category->update($request->all());
+
+
+        return redirect()->route('categories.index')
+                         ->with('success','Category updated successfully');
+    }
+   
+   
+   
+   
+   
+    public function destroy($id)
+    {
+        
+		$category = Category::find($id);
+		$category->delete();
+        return redirect()->route('categories.index')
+                         ->with('success','category deleted successfully');		
+		
+    }
+	
+	
+	
+	
+	
+}
