e9a42cc103c0957e6981d4a26abe0c4fc2c11005
janekingeniousio <janekbar5@interia.pl>
2018-12-03 15:03:27 +0000

    changes 03-12-2018 voucher contact form ok

--- !_development.php
+++ !_development.php
@@ -6,3 +6,9 @@ https://github.com/LaravelDaily/laravel-roles-permissions-manager
 USERS:
 Motorcycle Dealer
 ringwood@wp.pl
+
+//////////////////////////////done
+-select 2
+//////////////////////////////todo
+-search over relation
+-radius + paginatio
